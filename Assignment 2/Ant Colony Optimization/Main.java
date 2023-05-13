06f0ae423f78b10d02ec54908cbf452490a9f6bd
Paul <102551253+PaulPilane@users.noreply.github.com>
2023-05-13 14:04:18 +0000

    Deleted the KnapsackProblemACO.java

--- Assignment 2/Ant Colony Optimization/Main.java
+++ Assignment 2/Ant Colony Optimization/Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 public class Main {
     
     public static void main(String[] args) {
-        KnapsackProblemACO knapsack = new KnapsackProblemACO();   
 
         ArrayList<String> fileNames = readFromFile();
 
@@ -13,14 +12,7 @@ public class Main {
             String[] tokes = fileName.split("/");
             System.out.println("\u001B[32m#####################---->:: File Name: " + tokes[2] + " ::<----#####################\u001B[0m");
 
-            // Process the file Name
-            knapsack.processFile(fileName);
-
-            // Run the program
-            knapsack.solve();
-
-            // Print the results
-            knapsack.printItemsAndTotalWeight();
+           
         }
      
     }
